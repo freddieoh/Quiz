bdbaee9c49271d5e36ccc9d083528d7ec6517408
Fredrick Ohen <fredrickohen@gmail.com>
2017-01-31 12:13:19 -0600

    first commit

--- QuizViewController.h
+++ QuizViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  QuizViewController.h
 //  Quiz
 //
 //  Created by Fredrick Ohen on 1/31/17.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface QuizViewController : UIViewController
 
 @end
-
